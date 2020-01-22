58938982b5cf3f3f28f4c267e7fbc090da635a81
GediminasZukas <gediminas.zukas@vinted.com>
2020-01-22 11:00:24 +0200

    Implement portfolio edit and update functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -8,7 +8,7 @@ class PortfoliosController < ApplicationController
 	end
 
 	def create
-    	@portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+    	@portfolio_item = Portfolio.new(only_allowed_fields)
 
     	respond_to do |format|
       		if @portfolio_item.save
@@ -20,4 +20,26 @@ class PortfoliosController < ApplicationController
       		end
     	end
 	end
+
+	def edit
+		@portfolio_item = Portfolio.find(params[:id])
+	end
+
+	 def update
+		@portfolio_item = Portfolio.find(params[:id])
+
+    	respond_to do |format|
+     		if @portfolio_item.update(only_allowed_fields)
+      			format.html { redirect_to portfolios_path, notice: 'Portfolio item was successfully updated.' }
+        		format.json { render :show, status: :ok, location: @portfolio_item }
+      		else
+        		format.html { render :edit }
+       			format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+     		 end
+    	end
+  	end
+
+  	def only_allowed_fields
+		params.require(:portfolio).permit(:title, :subtitle, :body)
+  	end
 end
